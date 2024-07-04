540763f0d167f356e85b6900c209a4bd1d1298e5
star8795 <167267167+star8795@users.noreply.github.com>
2024-07-04 16:22:15 +0900

    Update mysql.sql
    최종 커밋

--- jsp_11_database_mysql/mysql.sql
+++ jsp_11_database_mysql/mysql.sql
@@ -11,6 +11,6 @@ CREATE TABLE IF NOT EXISTS member(
 	num INT PRIMARY KEY AUTO_INCREMENT,		-- 회원번호
 	id VARCHAR(300) NOT NULL UNIQUE,		-- 회원 아이디
     pass VARCHAR(300) NOT NULL,				-- 회원 비밀번호
-    name VARCHAR(50),						-- 이름
-    addr VARCHAR(300)						-- 주소
+    name VARCHAR(50),					-- 이름
+    addr VARCHAR(300)					-- 주소
 );
