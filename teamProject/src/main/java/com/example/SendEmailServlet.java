679ff6389f4943685eede00c980720287d2c36a3
star8 <star8@DESKTOP-UHBO80T>
2024-07-09 22:55:38 +0900

    커밋

---
+++ teamProject/src/main/java/com/example/SendEmailServlet.java
@@ -0,0 +1,61 @@
+package com.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class SendEmailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String crtfcNo = RandomStringUtils.randomNumeric(6);
+		
+		String host = "smtp.naver.com";
+		String senderEmail = "";	// 보내는 이의 메일 주소
+		String password = "";	// 보내는 이의 메일 비밀번호
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", 587);
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+		
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmail, password);
+            }
+        });
+		
+		try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            message.setSubject("이메일 인증번호");
+            message.setText("귀하의 이메일 인증번호는 " + crtfcNo + " 입니다.\n인증번호를 복사하여 입력해주세요.");
+
+            Transport.send(message);
+            request.getSession().setAttribute("authCode", crtfcNo);
+            response.getWriter().write("success");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            response.getWriter().write("fail");
+        }
+	}
+
+}
